5a6efcb772aab5fbe8211e9b0394810d9b3550df
nestorJaco <nestor.castro@cepa.gob.sv>
2022-10-12 07:58:22 -0600

    Cambios en CSS de productos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,43 @@ export class AppComponent {
     age: 54,
     avatar: 'https://www.w3schools.com/howto/img_avatar.png',
   }
-  names: string[] = ['Nico', 'Juli', 'Santi'];
+  
+  names: String[] = ['Nico', 'Juli', 'Santi'];
+  newName = '';
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción albumes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/glasses.jpg'
+    }
+  ]
+  
   toggleButton() {
     this.btnDisabled = !this.btnDisabled;
   }
@@ -33,6 +70,11 @@ export class AppComponent {
     this.person.name = element.value;
   
   }
-
-
+  addName() {
+    this.names.push(this.newName);
+    this.newName = '';
+  }
+  deleteName(index: number) {
+    this.names.splice(index, 1);
+  }
 }
